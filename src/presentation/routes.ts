e0c2b1783acfe7549f89750a11f477c4801b56fa
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2024-07-29 20:35:49 -0600

    cambios

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
+import { TodoRoutes } from "./todos/routes";
 
 export class AppRoutes {
   static get routes(): Router{
     const router = Router();
-
-    router.get('/api/todos', (req, res) => {});
+    router.use('/api/todos', TodoRoutes.routes);
 
     return router;
   }
---
+++ src/presentation/todos/controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+
+const todos = [
+  { id: 1, text: "Buy some milk", createdAt: new Date() },
+  { id: 2, text: "Pick up the kids", createdAt: new Date() },
+  { id: 3, text: "Get to work",  createdAt: new Date()}
+]
+
+export class TodosController {
+  public getTodos(req: Request, res: Response) {
+    res.json(todos)
+  }
+
+  public getTodoById(req: Request, res: Response) {
+    const id = req.params.id
+    if(isNaN(parseInt(id))) {
+      return res.status(400).json({ error: 'Missing todo id'})
+    }
+    const todo = todos.find(todo => todo.id === parseInt(id))
+    if(!todo) {
+      return res.status(404).json({ error: 'Todo not found'})
+    }
+    res.json(todo)
+  }
+}
