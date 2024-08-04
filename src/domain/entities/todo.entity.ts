830d98e6964a78578bd520c7d5c5a016dbd9d38f
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2024-08-04 17:05:01 -0600

    todo datasource

--- src/domain/entities/todo.entity.ts
+++ src/domain/entities/todo.entity.ts
@@ -8,4 +8,21 @@ export class TodoEntity {
   get isCompleted() {
     return !!this.completedAt;
   }
+
+  public static fromObject(obj: Record<string, any>): TodoEntity {
+    const { id, text, completedAt } = obj;
+
+    if (!id) throw new Error('Id is required');
+    if (!text) throw new Error('Text is required');
+
+    let newCompletedAt: Date | null = null;
+    if (completedAt) {
+      newCompletedAt = new Date(completedAt);
+      if (isNaN(newCompletedAt.getTime())) {
+        throw new Error('Invalid completedAt');
+      }
+    }
+
+    return new TodoEntity(id, text, newCompletedAt);
+  }
 }
