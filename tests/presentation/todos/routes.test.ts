01efeecc6087d1a4463e5c753d3a6f81b4da80e9
Jose Angel <ah@xdevelop.mx>
2024-08-07 20:59:36 -0600

    probando el endpoint de get todos insertando datos

--- tests/presentation/todos/routes.test.ts
+++ tests/presentation/todos/routes.test.ts
@@ -1,3 +1,4 @@
+import { prisma } from "../../../src/data/postgresql";
 import { testServer } from "../../testServer";
 import request from "supertest";
 
@@ -10,7 +11,30 @@ describe("Todos routes", () => {
     await testServer.stop();
   });
 
+  const todos = [{ text: "Todo 1" }, { text: "Todo 2" }];
+
   test("should return 200 when get all todos", async () => {
-    await request(testServer.app).get("/api/todos").expect(200);
+    await prisma.todo.deleteMany();
+    await prisma.todo.createMany({
+      data: todos,
+    });
+    const { body } = await request(testServer.app)
+      .get("/api/todos")
+      .expect(200);
+    expect(body[0]).toEqual(
+      expect.objectContaining({
+        text: todos[0].text,
+        completedAt: null,
+        id: expect.any(Number),
+      })
+    );
+    expect(body[1]).toEqual(
+      expect.objectContaining({
+        text: todos[1].text,
+        completedAt: null,
+        id: expect.any(Number),
+      })
+    );
+    expect(body).toHaveLength(2);
   });
 });
